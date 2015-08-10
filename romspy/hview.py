28b1e0e6b5dcf621791a246b30f953f80a9d21df
okadate@mbp <okada@civil.eng.osaka-u.ac.jp>
2015-08-10 01:12:48 -0300

    Add ini_diff and modify to set grdfile

--- romspy/hview.py
+++ romspy/hview.py
@@ -16,7 +16,7 @@ from basemap import basemap
 
 
 def hview(ncfile, vname, time=None, k=None, interval=None, fmt='%i', cff=1.0, 
-          cblabel=None, mapfile=None, cmap='jet', figfile=None):
+          cblabel=None, mapfile=None, cmap='jet', figfile=None, grdfile=None):
 
     JST = 'seconds since 1968-05-23 09:00:00 GMT'
 
@@ -24,8 +24,14 @@ def hview(ncfile, vname, time=None, k=None, interval=None, fmt='%i', cff=1.0,
     nc = netCDF4.Dataset(ncfile, 'r')
     print ncfile
 
-    x_rho = nc.variables['lon_rho'][0,:]-0.00449/2
-    y_rho = nc.variables['lat_rho'][:,0]-0.00546/2
+    if grdfile is not None:
+        grd = netCDF4.Dataset(grdfile, 'r')
+        x_rho = grd.variables['lon_rho'][0,:]-0.00449/2
+        y_rho = grd.variables['lat_rho'][:,0]-0.00546/2
+        grd.close()
+    else:
+        x_rho = nc.variables['lon_rho'][0,:]-0.00449/2
+        y_rho = nc.variables['lat_rho'][:,0]-0.00546/2
 
     if time is not None:
         t = netCDF4.date2num(time, JST)
@@ -50,7 +56,7 @@ def hview(ncfile, vname, time=None, k=None, interval=None, fmt='%i', cff=1.0,
     cbar = plt.colorbar(PC)
     if cblabel is not None:
         cbar.ax.set_ylabel(cblabel)
-    if cblabel is not None:
+    else:
         cbar.ax.set_ylabel(vname)
 
     # contour
@@ -80,7 +86,96 @@ def hview(ncfile, vname, time=None, k=None, interval=None, fmt='%i', cff=1.0,
         return ax
 
 
+def ini_diff(ncfile, vname, k=None, interval=None, fmt='%i', cff=1.0, 
+             cblabel=None, mapfile=None, cmap='jet', figfile=None, grdfile=None):
+
+    JST = 'seconds since 1968-05-23 09:00:00 GMT'
+
+    # read
+    nc = netCDF4.Dataset(ncfile, 'r')
+    print ncfile
+
+    if grdfile is not None:
+        grd = netCDF4.Dataset(grdfile, 'r')
+        x_rho = grd.variables['lon_rho'][0,:]-0.00449/2
+        y_rho = grd.variables['lat_rho'][:,0]-0.00546/2
+        grd.close()
+    else:
+        x_rho = nc.variables['lon_rho'][0,:]-0.00449/2
+        y_rho = nc.variables['lat_rho'][:,0]-0.00546/2
+
+    ocean_time = nc.variables['ocean_time'][0]
+    time = netCDF4.num2date(ocean_time, JST)
+    if k is not None:
+        var2d = nc.variables[vname][:,k-1,:,:] * cff
+    else:
+        var2d = nc.variables[vname][:,:,:] * cff
+    nc.close()
+    var2d = var2d[1,:,:] - var2d[0,:,:]
+
+    # pcolor
+    ax = plt.gca()
+    X, Y = np.meshgrid(x_rho, y_rho)
+    if interval is not None:
+        PC = ax.pcolor(X, Y, var2d, cmap=cmap, vmin=interval[0], vmax=interval[-1])
+    else:
+        PC = ax.pcolor(X, Y, var2d, cmap=cmap)
+    cbar = plt.colorbar(PC)
+    if cblabel is not None:
+        cbar.ax.set_ylabel(cblabel)
+    else:
+        cbar.ax.set_ylabel(vname)
+
+    # contour
+    matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
+    if interval is not None:
+        CF = plt.contour(X+0.00449, Y+0.00546, var2d, interval, colors='k', ls='-')
+    else:
+        CF = plt.contour(X+0.00449, Y+0.00546, var2d, colors='k', ls='-')
+    CF.clabel(fontsize=9, fmt=fmt, c='k')
+
+    # basemap
+    if mapfile is not None:
+        basemap(mapfile)
+    else:
+        basemap()
+
+    # finalize
+    plt.title(datetime.datetime.strftime(time,'%Y-%m-%d %H:%M:%S'))
+
+    if figfile is not None:
+        plt.savefig(figfile, bbox_inches='tight')
+        plt.close()
+    else:
+        return ax
+
+
+def _test1():
+    import datetime as dt
+    time = dt.datetime(2012, 3, 5, 12)
+    ncfile = '/Users/teruhisa/Dropbox/Data/OB500_fennelP/NL02/ob500_dia_0003.nc'
+    mapfile = '/Users/teruhisa/Dropbox/Data/deg_OsakaBayMap_okada.bln'
+    hview(ncfile, 'SOD', time, interval=np.arange(-30,1,5), mapfile=mapfile)
+    plt.savefig("hview_test.png")
+
+
+def _test2():
+    import datetime as dt
+    time = dt.datetime(2012, 8, 5, 0)
+    ncfile = '/Users/teruhisa/mnt/apps/OB500_fennelP/I4DVAR/ob500_ini.nc'
+    mapfile = '/Users/teruhisa/Dropbox/Data/deg_OsakaBayMap_okada.bln'
+    hview(ncfile, 'temp', time, 20, mapfile=mapfile, figfile="hview_test2.png")
+
+
+def _test3():
+    ncfile = '/Users/teruhisa/mnt/apps/OB500_fennelP/I4DVAR01/ob500_ini.nc'
+    mapfile = '/Users/teruhisa/Dropbox/Data/deg_OsakaBayMap_okada.bln'
+    ini_diff(ncfile, 'temp', 20, interval=np.arange(-1.0,1.1,1), mapfile=mapfile, figfile="hview_test3.png")
+
+
 if __name__ == '__main__':
+
+    _test3()
 
     """
     from hplot_stations import hplot_stations
@@ -103,10 +198,3 @@ if __name__ == '__main__':
     plt.savefig('../example/OB500/ob500_avg_temp_t0_k20_values.png', 
                 bbox_inches='tight')
     """
-
-    import datetime as dt
-    time = dt.datetime(2012, 3, 5, 12)
-    ncfile = '/Users/teruhisa/Dropbox/Data/OB500_fennelP/NL02/ob500_dia_0003.nc'
-    mapfile = '/Users/teruhisa/Dropbox/Data/deg_OsakaBayMap_okada.bln'
-    ax = hview(ncfile, 'SOD', time, interval=np.arange(-30,1,5), mapfile=mapfile)
-    plt.savefig("hview_test.png")
