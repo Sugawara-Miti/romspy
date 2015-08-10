325a422982e4b875b9a0f6b9ee832b939974af1e
okadate@mbp <okada@civil.eng.osaka-u.ac.jp>
2015-08-10 01:11:06 -0300

    Modify get_time to be able to correct time name

--- romspy/get_time.py
+++ romspy/get_time.py
@@ -9,17 +9,17 @@ import netCDF4
 JST = 'seconds since 1968-05-23 09:00:00 GMT'
 
 
-def get_time(ncfile, which='ends', tunit=JST):
+def get_time(ncfile, which='ends', tunit=JST, name='ocean_time'):
     nc = netCDF4.Dataset(ncfile, 'r')
     print ncfile
     if which == 'ends':
-        t = len(nc.dimensions['ocean_time'])
-        start = nc.variables['ocean_time'][0]
-        end = nc.variables['ocean_time'][t-1]
+        t = len(nc.dimensions[name])
+        start = nc.variables[name][0]
+        end = nc.variables[name][t-1]
         print netCDF4.num2date(start, tunit), 0
         print netCDF4.num2date(end, tunit), t-1
     elif which == 'all':
-        time = nc.variables['ocean_time'][:]
+        time = nc.variables[name][:]
         for t in range(len(time)):
             print netCDF4.num2date(time[t], JST), t
     else:
