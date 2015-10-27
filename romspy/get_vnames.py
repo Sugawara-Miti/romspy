118c777832fc9740ad8cbbd6224e499e89660ba8
okadate <okada@civil.eng.osaka-u.ac.jp>
2015-10-27 16:05:07 +0900

    Modify get_vnames

--- romspy/get_vnames.py
+++ romspy/get_vnames.py
@@ -6,11 +6,21 @@ import netCDF4
 __version__ = 0.1  # 2015-10-27
 
 
-def get_vnames(ncfile):
-    nc = netCDF4.Dataset(ncfile, 'r')
-    print ncfile
-    print nc.variables.keys()
-    nc.close()
+def get_vnames(ncfile, ndim=None):
+    if type(ncfile) == str:
+        nc = netCDF4.Dataset(ncfile, 'r')
+        print ncfile
+    else:
+        nc = ncfile
+    if ndim is not None:
+        varnames = []
+        for vname in nc.variables.keys():
+            if nc.variables[vname].ndim == ndim:
+                varnames.append(vname)
+        print varnames
+    else:
+        print nc.variables.keys()
 
 if __name__ == '__main__':
-    get_vnames('/Users/teruhisa/Dropbox/Data/ob500_ini_fennelP-6.nc')
+    get_vnames('Z:/apps/OB500P/case15/NL/ob500_dia_0004.nc', 4)
+
